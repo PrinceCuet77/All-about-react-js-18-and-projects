5d2bcd39d5757792e206298ab0ff2bb7aec9b28d
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2023-06-06 15:01:51 +0600

    Refactor the code base

--- 03-advanced-react/src/tutorial/10-useReducer/practice2/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/practice2/01-useReducer.jsx
@@ -12,10 +12,6 @@ const defaultState = {
 
 const ReducerBasics = () => {
   const [name, setName] = useState('')
-  // const [people, setPeople] = useState([])
-  // const [showModal, setShowModal] = useState(false)
-  // const [modalContent, setModalContent] = useState('')
-
   const [state, dispatch] = useReducer(recuder, defaultState)
 
   const handleSubmit = (e) => {
@@ -27,22 +23,11 @@ const ReducerBasics = () => {
       dispatch({ type: ADD_ITEM, payload: newPeople })
     } else {
       dispatch({ type: EMPTY_ITEM })
-      // setShowModal(true)
-      // setModalContent('Please, add item')
     }
   }
 
   const closeModal = () => {
     dispatch({ type: CLOSE_MODAL })
-  }
-
-  const removeItem = (id) => {
-    // setPeople(people.filter((person) => person.id !== id))
-    // setShowModal(true)
-    // const item = people.find((person) => person.id === id)
-    // setModalContent(`${item.name} is deleted`)
-
-    dispatch({ type: REMOVE_ITEM, payload: { id } })
   }
 
   return (
